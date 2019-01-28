3a894e1864eededb54368fdd9bf8d37fdc3f013a
Matthew Kelemen <mkelemen112@gmail.com>
2019-01-28 11:04:58 -0700

    Create ArrayTest.java

---
+++ src/main/webapp/Java/Model/ArrayTest.java
@@ -0,0 +1,43 @@
+package Model;
+
+public class ArrayTest {
+
+   @Override
+    public String toString(EmployeeConstraints ec) {
+
+
+
+       System.out.println("Contraint Parse Test");
+
+           System.out.println("Monday Avail:");
+       for (int i=0; i<=ec.getAvailableTimeMonday().length; i++) {
+           System.out.print(ec.getAvailableTimeMonday()[i]+" ");
+       }
+
+       System.out.println("Monday Pref:");
+       for (int i=0; i<=ec.getPreferredTimeMonday().length; i++) {
+           System.out.print(ec.getPreferredTimeMonday()[i]);
+       }
+
+       System.out.println("Tuesday Avail: "+ec.getAvailableTimeTuesday());
+       System.out.println("Tuesday Pref: "+ec.getPreferredTimeTuesday()+"\n");
+
+       System.out.println("Wednesday Avail: "+ec.getAvailableTimeWednesday());
+       System.out.println("Wednesday Pref: "+ec.getPreferredTimeWednesday()+"\n");
+
+       System.out.println("Thursday Avail: "+ec.getAvailableTimeThursday());
+       System.out.println("Thursday Pref: "+ec.getPreferredTimeThursday()+"\n");
+
+       System.out.println("Friday Avail: "+ec.getAvailableTimeFriday());
+       System.out.println("Friday Pref: "+ec.getPreferredTimeFriday()+"\n");
+
+       System.out.println("Saturday Avail: "+ec.getAvailableTimeSaturday());
+       System.out.println("Saturday Pref: "+ec.getPreferredTimeSaturday()+"\n");
+
+       System.out.println("Sunday Avail: "+ec.getAvailableTimeSunday());
+       System.out.println("Sunday Pref: "+ec.getPreferredTimeSunday()+"\n");
+
+   }
+
+
+}

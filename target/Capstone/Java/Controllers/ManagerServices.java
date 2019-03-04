ca43fb6e057eee435f903188757a4e9699bc3e62
Anthony Doucet <pleako@hotmail.com>
2019-03-04 11:41:22 -0700

    ManagerSettingsJSP and Controller part uno

--- target/Capstone/Java/Controllers/ManagerServices.java
+++ target/Capstone/Java/Controllers/ManagerServices.java
@@ -7,13 +7,40 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "ManagerServices")
+@WebServlet(name = "ManagerServices", urlPatterns ="/MangagerServices")
 public class ManagerServices extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String newShiftDay = request.getParameter("newShiftDay");
+        String message = request.getParameter("message");
+        String sunday = request.getParameter("sunday");
+        String monday = request.getParameter("monday");
+        String tuesday = request.getParameter("tuesday");
+        String wednesday = request.getParameter("wednesday");
+        String thursday = request.getParameter("thursday");
+        String friday = request.getParameter("friday");
+        String saturday = request.getParameter("saturday");
+
+        DBoperations db = new DBoperations();
+
+        if (newShiftDay!=null) {
+
+            String newShiftStart = request.getParameter("newShiftStart");
+            String newShiftEnd = request.getParameter("newShiftEnd");
+            //db.addShift(newShiftDay, newShiftStart, newShiftEnd);
+            request.setAttribute("message", "New shift added");
+            getServletContext().getRequestDispatcher("/WEB-INF/ManagerServices.jsp").forward(request, response);
+
+        }
 
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
     }
 }

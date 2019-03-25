1de3e173c66c69b34b60d60104103563a2d35c88
jasnsy <33563928+jasnsy@users.noreply.github.com>
2019-03-25 13:10:14 -0600

    Push

--- src/main/webapp/Java/Persistance/DBOperation.java
+++ src/main/webapp/Java/Persistance/DBOperation.java
@@ -194,5 +194,43 @@ public class DBOperation {
 
     }
 
+    public ArrayList<ShiftTemplate> getShiftTemplates(char type) {
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        Query query = session.createQuery("SELECT s FROM ShiftTemplate s WHERE s.type = :type");
+        query.setParameter("type", type);
+
+        ArrayList<ShiftTemplate> shiftList = new ArrayList<>(query.list());
+        session.getTransaction().commit();
+        session.close();
+
+        return shiftList;
+    }
+
+    public void updateShiftTemplate(temp.ShiftTemplate st) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.update(st);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void deleteShiftTemplate(temp.ShiftTemplate st) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.delete(st);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void addShiftTemplate(temp.ShiftTemplate st) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.save(st);
+        session.getTransaction().commit();
+        session.close();
+    }
 
 }

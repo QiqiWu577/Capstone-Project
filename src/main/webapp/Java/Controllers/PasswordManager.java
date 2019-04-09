f17cf8746a25e7a38f13b21bd0bd7bcb11ca910e
astrobandaid <36897151+astrobandaid@users.noreply.github.com>
2019-04-09 14:42:24 -0600

    Validate and PasswordManager JavaDoc

--- src/main/webapp/Java/Controllers/PasswordManager.java
+++ src/main/webapp/Java/Controllers/PasswordManager.java
@@ -1,8 +1,6 @@
 package Controllers;
 
 
-import Model.Employee;
-
 import javax.xml.bind.DatatypeConverter;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
@@ -11,8 +9,17 @@ import java.security.SecureRandom;
 import java.sql.*;
 import java.util.Random;
 
+/**
+ * PasswordManager.java - Class describing all attributes and operations for a PasswordManager object
+ *
+ * @author Matthew Kelemen
+ */
 public class PasswordManager {
 
+    /**
+     * Opens a connection to the MySQL database
+     * @return conn - A connection to the database
+     */
     private Connection getConnection() {
 
         Connection conn=null;
@@ -27,7 +34,11 @@ public class PasswordManager {
         return conn;
     }
 
-
+    /**
+     * Adds a user to the password/salt table in the database.
+     * @param empid - empId of the user to be added to the password table
+     * @param password - password that is to be hashed and added to the database
+     */
     public void addUser(int empid, String password) {
 
         PasswordManager pm = new PasswordManager();
@@ -69,6 +80,12 @@ public class PasswordManager {
 
     }
 
+    /**
+     * This is the method that verifies if the password entered on the login screen matches the password that is in the password table.
+     * @param empid
+     * @param verify
+     * @return
+     */
     public boolean getHashSalt(int empid, String verify) {
         boolean valid=false;
         String hash = null;
@@ -103,6 +120,11 @@ public class PasswordManager {
         return valid;
     }
 
+    /**
+     * Generates a salt that will be used to hash a password before being stored to the database.
+     * @return salt - random mix of characters that is used to hash the password
+     * @throws NoSuchAlgorithmException
+     */
     public static byte[] getSalt() throws NoSuchAlgorithmException
     {
 
@@ -114,7 +136,12 @@ public class PasswordManager {
         return salt;
     }
 
-
+    /**
+     * This method generates a hash based off of a password and salt.
+     * @param toHash - password that is to be hashed and added to database
+     * @param salt - salt that is to be added to password before password is hashed and stored
+     * @return generatedPassword - the hashed password is returned
+     */
     public String hash(String toHash, byte[] salt){
 
 
@@ -137,6 +164,13 @@ public class PasswordManager {
         return generatedPassword;
     }
 
+    /**
+     * This is a helper method that helps determine if the password entered and the password in the database are the same.
+     * @param hash - hash to be used to check passwords
+     * @param verify - password that is entered to be comared to hash
+     * @param salt - salt that is used to hash the password
+     * @return boolean - returns a boolean. True if password entered matches password in database. False if password entered does not match password in database.
+     */
     public boolean checkPassword(String hash, String verify, String salt){
 
         byte[] byteSalt;
@@ -147,13 +181,17 @@ public class PasswordManager {
         return hash.equals(generatedHash);
     }
 
+    /**
+     * This method generates a random password of 8 characters that will be used as the temporary password for new hires and reset passwords
+     * @return tempPass - a temporary String of 8 random characters. AlphaNumeric
+     */
     public String generatePassword(){
 
         final String PASS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         String tempPass="";
         Random rand = new Random();
 
-       // rand.nextInt();
+        // rand.nextInt();
 
         for (int i =0; i<=8; i++) {
             int n = rand.nextInt(35);
@@ -167,45 +205,4 @@ public class PasswordManager {
     }
 
 
-
-
-
-    public void updateScheduleEmployee(int s, Employee e) {
-        Connection conn=getConnection();
-        String sql = "INSERT INTO schedule_employee VALUES(?,?)";
-
-        System.out.println("test1");
-        System.out.println(s + " : "+ e.getEmpid());
-
-        try {
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setInt(1, s);
-            ps.setInt(2, e.getEmpid());
-            System.out.println(s + " : "+ e.getEmpid());
-            ps.executeUpdate();
-
-            ps.close();
-
-        } catch (SQLException e1) {
-            e1.printStackTrace();
-        } finally {
-
-            try {
-                conn.close();
-            } catch (SQLException e1) {
-                e1.printStackTrace();
-            }
-        }
-    }
-
-
-//    public void resetPassword(String username, ) {
-//
-//
-//
-//
-//
-//
-//
-//    }
 }
--- src/main/webapp/Java/Controllers/Validate.java
+++ src/main/webapp/Java/Controllers/Validate.java
@@ -11,9 +11,19 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+/**
+ * @author Matthew Kelemen
+ */
 @WebServlet(name = "Validate", urlPatterns = "/Validate")
 public class Validate extends HttpServlet {
 
+    /**
+     * Processes the request for user validation
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         DBOperation dbops = new DBOperation();
